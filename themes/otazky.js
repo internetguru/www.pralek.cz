5aca90c5324d422cbf18b5e45663392b71856611
igserver <pavel.petrzela@internetguru.cz>
2017-09-26 23:10:01 +0200

    gituserfiles autocommit

--- themes/otazky.js
+++ themes/otazky.js
@@ -1,41 +1,39 @@
 (function(win){
 
-  var lists  = document.getElementsByTagName("ol"),
-    otazky = [],
-    yes    = "Srozumitelně",
-    no     = "Nesrozumitelně",
-    debug  = true;
+  var yes = "Srozumitelně";
+  var no  = "Nesrozumitelně";
 
   function init() {
+    var lists = document.getElementsByTagName("ol");
     for(var i = 0; i < lists.length; i++) {
-      if(lists[i].classList.contains("otazky")) otazky.push(lists[i]);
-    }
-
-    for(var i = 0; i < otazky.length; i++) {
-      var items = otazky[i].getElementsByTagName("li");
+      if(!lists[i].classList.contains("otazky")) continue;
+      var items = lists[i].getElementsByTagName("li");
       for(var j = 0; j < items.length; j++) {
-        var yesButton = document.createElement("button");
-        yesButton.addEventListener("click", actionYes.bind(items[j].innerHTML), false);
-        var noButton = document.createElement("button");
-        noButton.addEventListener("click", actionNo.bind(items[j].innerHTML), false);
-        var span = document.createElement("span");
-        items[j].appendChild(span);
-        span.appendChild(yesButton);
-        span.appendChild(noButton);
-        yesButton.innerHTML = yes;
-        noButton.innerHTML = no;
+        addButtons(items[j]);
       }
     }
   }
+  
+  function addButtons(e) {
+    var yesButton = document.createElement("button");
+    yesButton.addEventListener("click", actionYes.bind(e.innerHTML), false);
+    var noButton = document.createElement("button");
+    noButton.addEventListener("click", actionNo.bind(e.innerHTML), false);
+    var span = document.createElement("span");
+    e.appendChild(span);
+    span.appendChild(yesButton);
+    span.appendChild(noButton);
+    yesButton.innerHTML = yes;
+    noButton.innerHTML = no;
+  }
 
   function actionYes(e) { action(e, this, 1); }
+
   function actionNo(e) { action(e, this, 0); }
   
   function action(e, otazka, value) {
     var button = e.target;
-    if(debug) {
-      alert("otazka: '" + otazka + "', hodnota: " + value + "");
-    } else {
+    if(typeof ga == "function") {
       ga('send', {
           'hitType': 'event',
           'eventCategory': 'dotaznik',
@@ -43,14 +41,17 @@
           'eventLabel': otazka,
           'eventValue': value
         });
+    } else {
+      alert("DEBUG: " + otazka + " [" + value + "]");
     }
     e.target.classList.add("selected");
-    var childs = e.target.parentNode.childNodes;
-    for(var i = 0; i < childs.length; i++) {
-      if(childs[i].nodeName.toLowerCase() == "button") childs[i].disabled = "disabled";
+    var nodes = e.target.parentNode.childNodes;
+    for(var i = 0; i < nodes.length; i++) {
+      if(nodes[i].nodeName.toLowerCase() != "button") continue;
+      nodes[i].disabled = "disabled";
     }
   }
 
-  if(typeof ga == "function" || debug) init();
+  init();
   
 })(window);
