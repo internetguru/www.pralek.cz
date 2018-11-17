c8c8bce2c82571e6b16ffc2fff950c5180f5fa77
igserver <pavel.petrzela@internetguru.cz>
2018-11-17 20:10:01 +0100

    gituserfiles autocommit

--- update_kw.sh
+++ update_kw.sh
@@ -9,15 +9,21 @@ labels="$(curl 'https://docs.google.com/spreadsheets/d/18hz-SdBnXhB4CumVAEzZU5E5
 
 MIN_COUNT=5
 AGREGATOR_XML="plugins/Agregator/Agregator.xml"
+INPUTVAR_XML="plugins/InputVar/InputVar.xml"
 sed -i '/for="/d' $AGREGATOR_XML 
+sed -i '/<data/d' $INPUTVAR_XML 
 
 while IFS=, read -r label count
 do
   label="$(getValue "$label")"
   count="$(getValue "$count")"
-  [[ $count -lt $MIN_COUNT ]] && continue;
+  if [[ $count -lt $MIN_COUNT ]]; then
+    sed -i "s/<fn id=\"replacenames\" fn=\"replace\">/<fn id=\"replacenames\" fn=\"replace\">\n    <data name=\"?clanky=$label#koutek\"><\/data>/" $INPUTVAR_XML
+    continue
+  fi
   normalizedLabel="$(echo "$label" | iconv -f utf8 -t ascii//TRANSLIT | tr " " "_")"
   sed -i "s/<\/Agregator>/  <doclist id=\"$normalizedLabel\" kw=\"$label\" for=\"clanky\" \/>\n<\/Agregator>/" $AGREGATOR_XML
+  sed -i "s/<fn id=\"replacenames\" fn=\"replace\">/<fn id=\"replacenames\" fn=\"replace\">\n    <data name=\"=$label\">=$normalizedLabel<\/data>/" $INPUTVAR_XML
 done <<< "$labels"
 
 export="$(curl 'https://docs.google.com/spreadsheets/d/18hz-SdBnXhB4CumVAEzZU5E5Rr6eUswvopMFb32QDPw/gviz/tq?tqx=out:csv&sheet=Export')"
