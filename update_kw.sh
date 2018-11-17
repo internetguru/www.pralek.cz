27b450de9659a0afa76fe7c4a0f724c92ba3fadf
igserver <pavel.petrzela@internetguru.cz>
2018-11-17 19:10:02 +0100

    gituserfiles autocommit

--- update_kw.sh
+++ update_kw.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-csv="$(curl 'https://docs.google.com/spreadsheets/d/18hz-SdBnXhB4CumVAEzZU5E5Rr6eUswvopMFb32QDPw/export?format=csv&id=18hz-SdBnXhB4CumVAEzZU5E5Rr6eUswvopMFb32QDPw&gid=0')"
+getValue() {
+  val="${1#\"}"
+  echo "${val%\"}"
+}
+
+labels="$(curl 'https://docs.google.com/spreadsheets/d/18hz-SdBnXhB4CumVAEzZU5E5Rr6eUswvopMFb32QDPw/gviz/tq?tqx=out:csv&sheet=Labels')"
 
 MIN_COUNT=5
 AGREGATOR_XML="plugins/Agregator/Agregator.xml"
-sed -i '/for="/' $AGREGATOR_XML 
+sed -i '/for="/d' $AGREGATOR_XML 
 
-while IFS=, read -r url label count null
+while IFS=, read -r label count
 do
+  label="$(getValue "$label")"
+  count="$(getValue "$count")"
   [[ $count -lt $MIN_COUNT ]] && continue;
+  normalizedLabel="$(echo "$label" | iconv -f utf8 -t ascii//TRANSLIT | tr " " "_")"
+  sed -i "s/<\/Agregator>/  <doclist id=\"$normalizedLabel\" kw=\"$label\" for=\"clanky\" \/>\n<\/Agregator>/" $AGREGATOR_XML
+done <<< "$labels"
+
+export="$(curl 'https://docs.google.com/spreadsheets/d/18hz-SdBnXhB4CumVAEzZU5E5Rr6eUswvopMFb32QDPw/gviz/tq?tqx=out:csv&sheet=Export')"
+
+while IFS=, read -r url labels
+do
+  url="$(getValue "$url")"
+  labels="$(getValue "$labels")"
   id="${url##*/}"
-  sed -i "s/<\/Agregator>/<doclist id=\"$normalizedLabel\" kw=\"$label\" for=\"clanky\" \/>\n<\/Agregator>/" $AGREGATOR_XML
-done <<< "$csv"
+  file="plugins/Agregator/koutek/$id.html"
+  sed -i "s/kw=\"[^\"]\+\"/kw=\"$labels\"/" "$file"
+done <<< "$(echo "$export" | tail -n+2)"
 
