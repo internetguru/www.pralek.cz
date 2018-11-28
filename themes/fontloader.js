e841fbd037608b0eef5cc2e5b02d52ff680831a3
admin <igcms@commit.com>
2018-11-28 21:17:12 +0000

    Admin

--- themes/fontloader.js
+++ themes/fontloader.js
@@ -1,7 +1,8 @@
 require("WebFont", function() {
   WebFont.load({
     google: {
-      families: ['Lato:400,300,400italic,700:latin-ext'],
+      families: ['Roboto:400,300,400italic,700:latin-ext',
+                 'Noto Sans:400,400italic,700:latin-ext'],
 //     families: ['Roboto'],
 //     urls: ['/lib/roboto/roboto.css'],
     },
