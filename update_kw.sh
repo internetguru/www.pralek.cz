dc1e042fe4f06a115a9a68a6621e8c33d3855444
igserver <pavel.petrzela@internetguru.cz>
2018-11-29 16:10:01 +0100

    gituserfiles autocommit

--- update_kw.sh
+++ update_kw.sh
@@ -14,21 +14,25 @@ sed -i '/<data/d' $INPUTVAR_XML
 sed -i '/<option /d' $INPUTVAR_XML
 sed -i '/gen="gen"/d' $URLHANDLER_XML
 
-
+currentIds="$(for f in $(ls -r1 plugins/Agregator/koutek/*.html); do f="${f##*/}"; echo "${f%.html}"; done)"
 export="$(curl 'https://docs.google.com/spreadsheets/d/18hz-SdBnXhB4CumVAEzZU5E5Rr6eUswvopMFb32QDPw/gviz/tq?tqx=out:csv&sheet=Export')"
 
-while IFS=, read -r url labels
+exportIds=""
+while IFS=, read -r id labels
 do
-  url="$(getValue "$url")"
+  id="$(getValue "$id")"
   labels="$(getValue "$labels")"
-  id="${url##*/}"
+  exportIds="${exportIds}\n${id}"
+  continue
   file="plugins/Agregator/koutek/$id.html"
   sed -i "s/kw=\"[^\"]\+\"/kw=\"$labels\"/" "$file"
   sed -i "s/class=\"completable\">/class=\"completable\">\n<option class=\"article\" value=\"$id\">$(hxselect -c "body > h" < "$file") #$id<\/option>/" $INPUTVAR_XML
   sed -i "s/<UrlHandler>/<UrlHandler>\n<redir gen=\"gen\" parName='s' parValue='$id'>$id?<\/redir>/" $URLHANDLER_XML
 done <<< "$(echo "$export" | tail -n+2)"
 
+vimdiff <(echo "$currentIds") <(echo -e "$exportIds")
 
+exit
 labels="$(curl 'https://docs.google.com/spreadsheets/d/18hz-SdBnXhB4CumVAEzZU5E5Rr6eUswvopMFb32QDPw/gviz/tq?tqx=out:csv&sheet=Labels')"
 
 sed -i "s/<fn id=\"replacenames\" fn=\"replace\">/<fn id=\"replacenames\" fn=\"replace\">\n    <data name=\"?clanky=nejctenejsi\"><\/data>/" $INPUTVAR_XML
