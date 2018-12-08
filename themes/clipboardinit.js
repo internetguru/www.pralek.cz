d642fd62018915617fd33af42979c33826586c3f
admin <igcms@commit.com>
2018-12-08 17:09:33 +0000

    Admin

--- themes/clipboardinit.js
+++ themes/clipboardinit.js
@@ -1,3 +1,7 @@
 require("ClipboardJS", function () {
   var clipboard = new ClipboardJS('.share');
+  clipboard.on('success', function(event) {
+    event.preventDefault()
+    return false
+  });
 })
