855013f0f945056407dec04c4657aad666abdf21
admin <igcms@commit.com>
2018-12-11 00:16:43 +0000

    Admin

--- themes/filter-focus.js
+++ themes/filter-focus.js
@@ -1,9 +1,9 @@
 (function () {
 
   var filters = document.querySelectorAll(".filter a")
-  var search = document.querySelector(".search input")
   for (var i = 0; i < filters.length; i++) {
       filters[i].addEventListener("click", function () {
+        var search = document.querySelector(".search input")
         search.focus()
         search.click()
         search.click()
@@ -12,7 +12,7 @@
   
   document.addEventListener ("keydown", function (event) {
     if (event.ctrlKey  &&  event.shiftKey  &&  event.keyCode === 70) {
-        search.focus()
+       document.querySelector(".search input").focus()
     }
   } )
 
