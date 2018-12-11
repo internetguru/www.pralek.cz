17369cb1a0e9e2791ba17dc0640a68052f69be41
admin <igcms@commit.com>
2018-12-11 00:12:41 +0000

    Admin

--- themes/filter-focus.js
+++ themes/filter-focus.js
@@ -9,5 +9,11 @@
         search.click()
       }, false)
   }
+  
+  document.addEventListener ("keydown", function (event) {
+    if (event.ctrlKey  &&  event.shiftKey  &&  event.keyCode === 70) {
+        document.querySelector(".search input").focus()
+    }
+  } )
 
 })()
