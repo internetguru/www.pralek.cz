42b58ad33529b909e24d9c6b491f64067aacf982
admin <igcms@commit.com>
2018-12-27 08:07:55 +0000

    Admin

--- themes/filter-focus.js
+++ themes/filter-focus.js
@@ -1,32 +1,12 @@
 (function () {
-
-    var Config = {}
-    Config.defaultChangeText = "Změnit"
-
+    
     var selectedTag = document.querySelectorAll("a.tag.nowarning[href^='#']")
-    var filters = document.querySelectorAll(".filter")
-    for (var i = 0; i < filters.length; i++) {
-        if (selectedTag.length > 0) {
-            var tag = selectedTag[0].cloneNode(true)
-            tag.removeAttribute("href")
-            filters[i].appendChild(tag)
-        }
-        var changeLink = document.createElement("a")
-        changeLink.textContent = Config.defaultChangeText
-        changeLink.className = "button"
-        changeLink.addEventListener("click", function () {
-            var search = document.querySelector(".search input")
-            search.focus()
-            search.click()
-            search.click()
-        }, false)
-        filters[i].appendChild(changeLink)
+    if (selectedTag.length <= 0) {
+        return
     }
-
-    document.addEventListener ("keydown", function (event) {
-        if (event.ctrlKey  &&  event.shiftKey  &&  event.keyCode === 70) {
-            document.querySelector(".search input").focus()
-        }
-    } )
+    var h2 = document.getElementById("clanky")
+    var tag = selectedTag[0].cloneNode(true)
+    tag.removeAttribute("href")
+    h2.appendChild(tag)
 
 })()
