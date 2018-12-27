e91cd70b433a4a77c8cc1ecec0564fa65bdf03b7
admin <igcms@commit.com>
2018-12-27 10:59:49 +0000

    Admin

--- themes/filter-focus.js
+++ themes/filter-focus.js
@@ -1,16 +1,12 @@
 (function () {
 
-    require("IGCMS.Completable", function () {
-    
-        var selectedTag = document.querySelectorAll("a.tag.nowarning[href^='#']")
-        if (selectedTag.length <= 0) {
-            return
-        }
-        var h2 = document.getElementById("clanky")
-        var tag = selectedTag[0].cloneNode(true)
-        tag.removeAttribute("href")
-        h2.appendChild(tag)
-
-    })
+  var selectedTag = document.querySelectorAll("a.tag.nowarning[href^='#']")
+  if (selectedTag.length <= 0) {
+      return
+  }
+  var h2 = document.getElementById("clanky")
+  var tag = selectedTag[0].cloneNode(true)
+  tag.removeAttribute("href")
+  h2.appendChild(tag)
 
 })()
