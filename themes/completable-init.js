e7ab4c54bab5acf504776445375eb37c4852188c
admin <igcms@commit.com>
2019-01-02 16:26:42 +0000

    Admin

--- themes/completable-init.js
+++ themes/completable-init.js
@@ -1,19 +1,13 @@
 (function() {
   require("IGCMS.Completable", function() {
     IGCMS.Completable.init({
-      selectSelector: 'select[name="Admin"]',
-      placeholder: "Vybrat soubor",
-      sendFormText: "Odeslat formulář",
-      sendFormClass: "sendform",
-      keyboardShortcut: "ctrl+p"
+      selector: '#header select.completable',
+      placeholder: "Co Vás zajímá? (Ctrl+Shift+F)",
+      defaultChangeText: "Změnit",
+      filterSelector: ".filter",
+      sendFormText: "Hledat na Google",
+      sendFormClass: "google",
+      keyboardshortcut: "ctrl+shif+s"
     })
-//     IGCMS.Completable.init({
-//       selector: '#header select.completable',
-//       placeholder: "Co Vás zajímá? (Ctrl+Shift+F)",
-//       defaultChangeText: "Změnit",
-//       filterSelector: ".filter",
-//       sendFormText: "Hledat na Google",
-//       sendFormClass: "google"
-//     })
   })
 })()
