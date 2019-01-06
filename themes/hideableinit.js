32630ab50431229c353c1eefb23684d74bbadcce
admin <igcms@commit.com>
2019-01-06 15:34:28 +0000

    Admin

---
+++ themes/hideableinit.js
@@ -0,0 +1,8 @@
+require("IGCMS.Copyable", function () {
+  require("IGCMS.Hideable", function () {
+    IGCMS.Hideable.init({
+      expandTitle: "Rozbalit",
+      collapseTitle: "Sbalit",
+    })
+  })
+})
