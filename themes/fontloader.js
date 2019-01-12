421400f0567007a39e6b0eb5145304bc574341fb
admin <igcms@commit.com>
2019-01-12 15:59:22 +0000

    Admin

--- themes/fontloader.js
+++ themes/fontloader.js
@@ -2,7 +2,9 @@ require("WebFont", function() {
   WebFont.load({
     google: {
       families: ['Cabin:400,400italic,500,700:latin-ext',
-                 'Rubik:400,400italic,500,700:latin-ext'],
+                 'Rubik:400,400italic,500,700:latin-ext',
+                 'Material Icons'],
+      urls: ['https://fonts.googleapis.com/icon?family=Material+Icons']
 //     families: ['Roboto'],
 //     urls: ['/lib/roboto/roboto.css'],
     },
