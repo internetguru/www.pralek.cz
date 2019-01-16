c8c17ba1cb061f2fe0f0c5ed3b728f7dcff27bba
admin <igcms@commit.com>
2019-01-16 16:06:57 +0000

    Admin

--- themes/moreableinit.js
+++ themes/moreableinit.js
@@ -2,6 +2,6 @@ require("IGCMS.Moreable", function () {
   IGCMS.Moreable.init({
     parent: ".part:last-child > .list.multiple:last-child > div",
     moreText: "Zobrazit další články",
-    leftText = "zbývá %s"
+    leftText = "zbývají %s"
   })
 })
