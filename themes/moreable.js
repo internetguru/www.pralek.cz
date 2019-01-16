641ea4feb2dd2ab7b2cf791548acd0776cd2e541
admin <igcms@commit.com>
2019-01-16 15:56:28 +0000

    Admin

--- themes/moreable.js
+++ themes/moreable.js
@@ -41,7 +41,7 @@
           moreLink.textContent = Config.moreText
           moreLink.addEventListener("click", showMore, false)
           var left = document.createElement("span")
-          left.innerHTML = hiddenItems.length - Config.displayStep
+          left.innerHTML = hiddenItems.length
           moreLink.appendChild(left)
           wrapper.appendChild(moreLink)
           parent.appendChild(wrapper)
