30532a477c55a28da2d49732696915cbd1130143
admin <igcms@commit.com>
2019-05-03 00:15:50 +0000

    Admin

--- themes/feedback.js
+++ themes/feedback.js
@@ -59,7 +59,7 @@
         var questionInput = getElm("textarea")
         
         questionLabel.setAttribute("for", "feedback-text")
-        questionInput.setAttribute("pattern", ".*\w+.*")
+        questionInput.setAttribute("pattern", "\s*\w+\s*")
         questionDt.appendChild(questionLabel)
         questionInput.id = "feedback-text"
         questionInput.setAttribute("placeholder", placeholder)
