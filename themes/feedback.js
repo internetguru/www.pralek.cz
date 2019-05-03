7fc54102b3660cbd40569b85255d1247263549df
admin <igcms@commit.com>
2019-05-03 00:23:18 +0000

    Admin

--- themes/feedback.js
+++ themes/feedback.js
@@ -59,7 +59,7 @@
         var questionInput = getElm("textarea")
         
         questionLabel.setAttribute("for", "feedback-text")
-        questionInput.setAttribute("pattern", "^.*[^\s]+.*$")
+        questionInput.setAttribute("pattern", ".*\w{2,}.*")
         questionInput.oninput = function () {
           questionInput.setCustomValidity("")
           questionInput.reportValidity()
