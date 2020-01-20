f8d8ad597a3859bf6d7537b69d5ca9a19dab2422
igserver <pavel.petrzela@internetguru.cz>
2020-01-20 13:15:06 +0100

    getEmail.sh check date

--- getEmail.sh
+++ getEmail.sh
@@ -5,7 +5,11 @@
   && exit 1
 
 id="$(hxselect -c "body > h::attr(id)" < "$1")"
-ctime="$(date -d "$(hxselect -c "body > h::attr(ctime)" < "$1")" +"%e. -%m %Y" \
+rawctime="$(hxselect -c "body > h::attr(ctime)" < "$1")"
+[[ "$(date +"%Y-%m-%d")" != "$rawctime" ]] \
+  && echo "article ctime must be today" \
+  && exit 1
+ctime="$(date -d "$rawctime" +"%e. -%m %Y" \
   | sed 's/-01/ledna/; s/-02/února/; s/-03/března/; s/-04/dubna/; s/-05/května/; s/-06/června/; s/-07/července/; s/-08/srpna/; s/-09/září/; s/-10/října/; s/-11/listopadu/; s/-12/prosince/')"
 heading="$(hxselect -c "body > h" < "$1")"
 description="$(hxselect -c "body > h + desc" < "$1")"
