151c48a27f27c4d86f30cbb6675cf3f35481bec4
pavel <igcms@commit.com>
2020-04-11 11:36:53 +0000

    Admin

--- themes/copyableinit.js
+++ themes/copyableinit.js
@@ -1,9 +1,5 @@
 require("IGCMS.Copyable", () => {
-  window.setTimeout(() => {
-    require("IGCMS.Hideable", () => {
-      IGCMS.Copyable.init({
-        selectTitle: "Zkopírovat odkaz"
-      })
-    })
-  }, 500)
+  IGCMS.Copyable.init({
+    selectTitle: "Zkopírovat odkaz"
+  })
 })
