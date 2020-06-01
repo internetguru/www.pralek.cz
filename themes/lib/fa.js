21cf8b409a3866c04cbb133bcc2f50ff4609b06f
pavel <igcms@commit.com>
2020-06-01 08:36:54 +0000

    Admin

---
+++ themes/lib/fa.js
@@ -0,0 +1,26 @@
+FontAwesomeConfig = {
+  observeMutations: true
+};
+
+(() => {
+    let icons = document.querySelectorAll('*[class*="fa-"]')
+    let iconsCache = {}
+    icons.forEach((icon) => {
+      const name = icon.className.match(/fa-([^ ]+)/)[1]
+      iconsCache.name = icon.className
+      const svg = document.createElemnt("svg")
+      svg.innerHTML = `<use xlink:href="#${name}"></use>`
+      icon.parentNode.replaceChild(svg, icon)
+    })
+    Object.entries(iconCache).forEach(([name, cls]) => {
+      let symbol = document.createElement("span")
+      symbol.className = cls
+      symbol.setAttribute("data-fa-symbol", name)
+      document.body.appendChild(symbol)
+    })
+    var script = document.createElement("script")
+    script.type = "text/javascript"
+    script.setAttribute("data-search-pseudo-elements", "")
+    script.src = "https://kit.fontawesome.com/b0c4047774.js"
+    document.head.appendChild(script)
+})()
