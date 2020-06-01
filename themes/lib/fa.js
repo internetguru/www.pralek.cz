9052bf58dd9b3b94951760093a23e228ac120050
pavel <igcms@commit.com>
2020-06-01 08:50:31 +0000

    Admin

--- themes/lib/fa.js
+++ themes/lib/fa.js
@@ -4,20 +4,21 @@ FontAwesomeConfig = {
 
 (() => {
     let icons = document.querySelectorAll('*[class*="fa-"]')
-    let iconsCache = {}
+    let iconsCache = []
     icons.forEach((icon) => {
-      const name = icon.className.match(/fa-([^ ]+)/)[1]
-      iconsCache.name = icon.className
+      const name = icon.className.match(/fa-(?!fw)([^ ]+)/)[1]
+      iconsCache[name] = icon.className
       const svg = document.createElement("svg")
-      svg.innerHTML = `<use xlink:href="#${name}"></use>`
+      svg.className = "fa-symbol"
+      svg.innerHTML = '<use xlink:href=#' + name + '></use>'
       icon.parentNode.replaceChild(svg, icon)
     })
-    Object.entries(iconsCache).forEach(([name, cls]) => {
+    for (let name in iconsCache) {
       let symbol = document.createElement("span")
-      symbol.className = cls
+      symbol.className = iconsCache[name]
       symbol.setAttribute("data-fa-symbol", name)
       document.body.appendChild(symbol)
-    })
+    }
     var script = document.createElement("script")
     script.type = "text/javascript"
     script.setAttribute("data-search-pseudo-elements", "")
