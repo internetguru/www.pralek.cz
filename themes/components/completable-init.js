014be7966420cd81733162ae4cfc051d43733381
pavel <igcms@commit.com>
2020-06-24 10:00:52 +0000

    Admin

--- themes/components/completable-init.js
+++ themes/components/completable-init.js
@@ -2,9 +2,10 @@
   require("IGCMS.Completable", function () {
     document.body.insertAdjacentHTML('beforeend',  '<span data-fa-symbol="tag" class="fas fa-tag"></span><span data-fa-symbol="doc" class="far fa-file-alt"></span>')
     IGCMS.Completable.init({
+      selectSelector: '#header select.completable',
       placeholder: "Co Vás zajímá? (Ctrl+Shift+F)",
       defaultChangeText: "Vyhledat",
-      filterSelector: ".filter",
+      // filterSelector: ".filter",
       sendFormText: "Hledat na Google",
       sendFormClass: "google",
       keyboardShortcut: "ctrl+shift+f",
