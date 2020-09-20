ab984280dd92b45181d38128bba8edfd1eeff188
pavel <igcms@commit.com>
2020-09-20 09:29:01 +0000

    Admin

--- themes/copyableinit.js
+++ themes/copyableinit.js
@@ -1,6 +1,6 @@
 require("IGCMS.Copyable", () => {
   IGCMS.Copyable.init({
     selectTitle: "Zkopírovat odkaz",
-    buttonHTML: "<span class='fas fa-copy'></span>"
+    buttonHTML: "<span class='fas fa-fw fa-copy'></span> Zkopírovat odkaz"
   })
 })
