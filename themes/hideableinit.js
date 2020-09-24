cbb88747f5f11589860c9471bd6e9fce08c788bd
pavel <igcms@commit.com>
2020-09-24 13:16:54 +0000

    Admin

--- themes/hideableinit.js
+++ themes/hideableinit.js
@@ -1,8 +1,6 @@
-require("IGCMS.Copyable", function () {
   require("IGCMS.Hideable", function () {
     IGCMS.Hideable.init({
       expandTitle: "Sdílet",
       collapseTitle: "Skrýt",
     })
   })
-})
