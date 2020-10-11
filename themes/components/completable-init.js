28304c7f13cffbafcd6292c45b8d69cc7c05d78e
pavel <igcms@commit.com>
2020-10-11 04:39:58 +0000

    Admin

--- themes/components/completable-init.js
+++ themes/components/completable-init.js
@@ -18,6 +18,17 @@
           .replace(/ - (.*)$/, "</br><span class='fp-secondary'>$1</span>")
       },
       onSend: (navig, currentFile, closeNavig) => {
+        closeNavig()
+        let path = ""
+        if (/^stitky/.test(currentFile.value)) {
+          //path = `/?usp=navig&stitek=${currentFile.value.match(/(?<=stitky\/).*/)[0]}#clanky`
+        } else if (/^clanky/.test(currentFile.value)) {
+          //path = `/${currentFile.value.match(/(?<=clanky\/).*/)[0]}?usp=navig`
+        } else {
+          path = "https://www.google.com/search?sitesearch=www.pralek.cz&amp;q=" + currentFile.value
+        }
+        console.log(path)
+        window.location = path        
       },
       sendForm: false
     })
